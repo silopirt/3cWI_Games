bcb87a5b1a1cf3f4378a47091032fb66ac68f566
silopirt <julian.sonderegger2005@gmail.com>
2022-03-09 09:35:29 +0100

    first game

---
+++ src/at/jul/games/firstgame/Rectangles.java
@@ -0,0 +1,35 @@
+package at.jul.games.firstgame;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.tests.AnimationTest;
+
+public class Rectangles extends BasicGame {
+    public Rectangles(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        graphics.drawRect(500,200,30,40);
+    }
+
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
+            container.setDisplayMode(800,600,false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+}
