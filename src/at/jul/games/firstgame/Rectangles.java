b2b7c9966ba15d112bf33a57793d5ac8d3e077b8
silopirt <julian.sonderegger2005@gmail.com>
2022-03-11 11:26:07 +0100

    Rectangle moves

--- src/at/jul/games/firstgame/Rectangles.java
+++ src/at/jul/games/firstgame/Rectangles.java
@@ -4,6 +4,9 @@ import org.newdawn.slick.*;
 import org.newdawn.slick.tests.AnimationTest;
 
 public class Rectangles extends BasicGame {
+    private float x;
+    private float y;
+
     public Rectangles(String title) {
         super(title);
     }
@@ -14,13 +17,20 @@ public class Rectangles extends BasicGame {
     }
 
     @Override
-    public void update(GameContainer gameContainer, int i) throws SlickException {
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
+        this.x+=(float) delta /8;
+        this.y+=(float) delta /8;
+
+        if(this.x+100 >= gameContainer.getWidth()|| this.y+100 >= gameContainer.getHeight()){
+            this.x = 0;
+            this.y = 0;
+        }
 
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        graphics.drawRect(500,200,30,40);
+        graphics.drawRect(this.x,this.y,100,100);
     }
 
     public static void main(String[] argv) {
