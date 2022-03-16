fa2d6518edc614a38f2df6445fe93ba7c02f958b
silopirt <julian.sonderegger2005@gmail.com>
2022-03-16 09:31:06 +0100

    Objectsdemo

--- src/at/jul/games/objectsdemo/Objectsdemo.java
+++ src/at/jul/games/objectsdemo/Objectsdemo.java
@@ -5,10 +5,14 @@ import org.newdawn.slick.*;
 public class Objectsdemo extends BasicGame {
     private float x_rect;
     private float y_rect;
+    private boolean isRectangleMovingRight = true;
+    private boolean isRectangleMovingDown = true;
 
     private float y_circle;
+    private boolean isCircleGoingDown = true;
 
     private float x_oval;
+    private boolean isOvalMovingRight = true;
 
 
     public Objectsdemo(String title) {
@@ -22,21 +26,59 @@ public class Objectsdemo extends BasicGame {
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
-        this.x_rect +=(float) delta/8;
-        this.y_rect +=(float) delta/8;
+        if(isRectangleMovingDown){
+            this.y_rect +=(float) delta/8;
+            if(this.y_rect + 100 >= gameContainer.getHeight()){
+                this.isRectangleMovingDown = false;
+            }
+        }else {
+            this.y_rect -= (float) delta/8;
+            if(y_rect<=0){
+                this.isRectangleMovingDown =true;
+            }
+        }
+        if(isRectangleMovingRight){
+            this.x_rect +=(float) delta/8;
+            if(this.x_rect + 100 >= gameContainer.getWidth()){
+                this.isRectangleMovingRight = false;
+            }
+        }else {
+            this.x_rect -= (float) delta/8;
+            if(x_rect<=0){
+                this.isRectangleMovingRight =true;
+            }
+        }
 
-        this.y_circle += (float) delta/8;
-        if(this.y_circle + 50 >= gameContainer.getHeight()){
-            while (this.y_circle >= 0) {
-                this.y_circle -= (float) delta / 8;
+
+        if (isCircleGoingDown){
+            this.y_circle += (float) delta/8;
+            if (this.y_circle + 50 >= gameContainer.getHeight()){
+                this.isCircleGoingDown= false;
+            }
+        } else {
+            this.y_circle -= (float) delta/8;
+            if (this.y_circle<= 0){
+                this.isCircleGoingDown= true;
+            }
+        }
+        if (isOvalMovingRight){
+            this.x_oval += (float) delta/8;
+            if (this.x_oval+100 >= gameContainer.getWidth()){
+                this.isOvalMovingRight= false;
+            }
+        } else {
+            this.x_oval -= (float) delta/8;
+            if (this.x_oval<= 0){
+                this.isOvalMovingRight= true;
             }
         }
     }
 
+
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
         graphics.drawRect(this.x_rect, this.y_rect,100,100);
-        graphics.drawOval(this.x_oval,100,100,200);
+        graphics.drawOval(this.x_oval,0,100,200);
         graphics.drawOval(0,this.y_circle,50,50);
     }
     public static void main(String[] argv) {
